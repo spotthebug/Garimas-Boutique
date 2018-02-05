813fa83e99f1f126709ba9653ce5e39875aad7db
spotthebug <gjain1@scu.edu>
2018-02-05 12:00:58 -0800

    Refactored dynamically adding image code.

--- js/main.js
+++ js/main.js
@@ -11,49 +11,57 @@ var addTopsTwo = document.getElementById('four');
 var addOuterwearOne = document.getElementById('five');
 var addOuterwearTwo = document.getElementById('six');
 
+// Array referring to dresses
+dressColumnOne = ['<img class="image-sizing"src="../assets/bluecocktail.jpg" alt="Black dress with contrasting trimmings">', '<img class="image-sizing"src="../assets/blueprom.jpg" alt="White colored prom dress with blue lace bodice.">', '<img class="image-sizing"src="../assets/floraldress.jpg" alt="Chic floral dress.">'];
+dressColumnTwo = ['<img class="image-sizing"src="../assets/promdress.jpg" alt="Prom dress">', '<img class="image-sizing"src="../assets/shiftdress.jpg" alt="Black shift dress">','<img class="image-sizing"src="../assets/denimdress.jpg" alt="Yellow dress">'];
+
+// Array referring to tops
+topsColumnOne = ['<img class="image-sizing"src="../assets/pearls.jpg" alt="Blue top with pearls.">', '<img class="image-sizing"src="../assets/blackcroptop.jpg" alt="Black crop top.">','<img class="image-sizing"src="../assets/CamisaChiffon.jpg" alt="Chiffon top.">'];
+topsColumnTwo = ['<img class="image-sizing"src="../assets/blousesv-neck.jpg" alt="V-neck blouse">','<img class="image-sizing"src="../assets/peachblouse.jpg" alt="Peach blouse.">','<img class="image-sizing"src="../assets/limegreentop.jpg" alt="Lime green top.">'];
+
+// Array referring to outerwear
+outerwearColumnOne = ['<img class="image-sizing"src="../assets/chic.jpeg" alt="Jacket with gold buttons">', '<img class="image-sizing"src="../assets/patagonia.jpg" alt="Wilderness wear">', '<img class="image-sizing"src="../assets/leatherjacket.jpeg" alt="Chic floral dress.">'];
+outerwearColumnTwo = ['<img class="image-sizing"src="../assets/fur.jpg" alt="Prom dress">', '<img class="image-sizing"src="../assets/velvet.jpg" alt="Velvet Jackets">', '<img class="image-sizing"src="../assets/burberry.jpeg" alt="Burberry jacket">'];
+
+// Add dresses dynamically to html
 dressLink.addEventListener("click", function() {
-  addDressOne.innerHTML += '<img class="image-sizing"src="../assets/bluecocktail.jpg" alt="Black dress with contrasting trimmings">';
-  addDressOne.innerHTML += '<button class="button">Add To Cart</button>';
-  addDressOne.innerHTML += '<img class="image-sizing"src="../assets/blueprom.jpg" alt="White colored prom dress with blue lace bodice.">';
-  addDressOne.innerHTML += '<button class="button">Add To Cart</button>';
-  addDressOne.innerHTML += '<img class="image-sizing"src="../assets/floraldress.jpg" alt="Chic floral dress.">';
-  addDressOne.innerHTML += '<button class="button">Add To Cart</button>';
-  addDressTwo.innerHTML += '<img class="image-sizing"src="../assets/promdress.jpg" alt="Prom dress">';
-  addDressTwo.innerHTML += '<button class="button">Add To Cart</button>';
-  addDressTwo.innerHTML += '<img class="image-sizing"src="../assets/shiftdress.jpg" alt="Black shift dress">';
-  addDressTwo.innerHTML += '<button class="button">Add To Cart</button>';
-  addDressTwo.innerHTML += '<img class="image-sizing"src="../assets/denimdress.jpg" alt="Yellow dress">';
-  addDressTwo.innerHTML += '<button class="button">Add To Cart</button>';
+  for (var i = 0; i < dressColumnOne.length; i++) {
+    addDressOne.innerHTML += dressColumnOne[i];
+    addDressOne.innerHTML += '<button class="button">Add To Cart</button>';
+  };
+
+  for (var i = 0; i < dressColumnTwo.length; i++) {
+    addDressTwo.innerHTML += dressColumnTwo[i];
+    addDressTwo.innerHTML += '<button class="button">Add To Cart</button>';
+  }
+
 });
 
+// Add tops dynamically to html
 topLink.addEventListener("click", function() {
-  addTopsOne.innerHTML += '<img class="image-sizing"src="../assets/pearls.jpg" alt="Blue top with pearls.">';
-  addTopsOne.innerHTML += '<button class="button">Add To Cart</button>';
-  addTopsOne.innerHTML += '<img class="image-sizing"src="../assets/blackcroptop.jpg" alt="Black crop top.">';
-  addTopsOne.innerHTML += '<button class="button">Add To Cart</button>';
-  addTopsOne.innerHTML += '<img class="image-sizing"src="../assets/CamisaChiffon.jpg" alt="Chiffon top.">';
-  addTopsOne.innerHTML += '<button class="button">Add To Cart</button>';
-  addTopsTwo.innerHTML += '<img class="image-sizing"src="../assets/blousesv-neck.jpg" alt="V-neck blouse">';
-  addTopsTwo.innerHTML += '<button class="button">Add To Cart</button>';
-  addTopsTwo.innerHTML += '        <img class="image-sizing"src="../assets/peachblouse.jpg" alt="Peach blouse.">';
-  addTopsTwo.innerHTML += '<button class="button">Add To Cart</button>';
-  addTopsTwo.innerHTML += '<img class="image-sizing"src="../assets/limegreentop.jpg" alt="Lime green top.">';
-  addTopsTwo.innerHTML += '<button class="button">Add To Cart</button>';
+  for (var i = 0; i < topsColumnOne.length; i++) {
+    addTopsOne.innerHTML += topsColumnOne[i];
+    addTopsOne.innerHTML += '<button class="button">Add To Cart</button>';
+  };
+
+  for (var i = 0; i < topsColumnTwo.length; i++) {
+    addTopsTwo.innerHTML += topsColumnTwo[i];
+    addTopsTwo.innerHTML += '<button class="button">Add To Cart</button>';
+  };
+
 });
 
 outerwearLink.addEventListener("click", function() {
-  addOuterwearOne.innerHTML += '<img class="image-sizing"src="../assets/chic.jpeg" alt="Jacket with gold buttons">';
-  addOuterwearOne.innerHTML += '<button class="button">Add To Cart</button>';
-  addOuterwearOne.innerHTML += '<img class="image-sizing"src="../assets/patagonia.jpg" alt="Wilderness wear">';
-  addOuterwearOne.innerHTML += '<button class="button">Add To Cart</button>';
-  addOuterwearOne.innerHTML += '<img class="image-sizing"src="../assets/leatherjacket.jpeg" alt="Chic floral dress.">';
-  addOuterwearOne.innerHTML += '<button class="button">Add To Cart</button>';
-  addOuterwearTwo.innerHTML += '<img class="image-sizing"src="../assets/fur.jpg" alt="Prom dress">';
-  addOuterwearTwo.innerHTML += '<button class="button">Add To Cart</button>';
-  addOuterwearTwo.innerHTML += '<img class="image-sizing"src="../assets/velvet.jpg" alt="Velvet Jackets">';
-  addOuterwearTwo.innerHTML += '<button class="button">Add To Cart</button>';
-  addOuterwearTwo.innerHTML += '<img class="image-sizing"src="../assets/burberry.jpeg" alt="Burberry jacket">';
-  addOuterwearTwo.innerHTML += '<button class="button">Add To Cart</button>';
-})
+  for (var i = 0; i < outerwearColumnOne.length; i++) {
+    addOuterwearOne.innerHTML += outerwearColumnOne[i];
+    addOuterwearOne.innerHTML += '<button class="button">Add To Cart</button>';
+  };
+
+  for (var i = 0; i < outerwearColumnTwo.length; i++) {
+    addOuterwearTwo.innerHTML += outerwearColumnTwo[i];
+    addOuterwearTwo.innerHTML += '<button class="button">Add To Cart</button>';
+  };
+
+});
 
 
