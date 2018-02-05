3a3efe3ae67aaa684d67b19e6902844935f72108
spotthebug <gjain1@scu.edu>
2018-02-05 08:12:29 -0800

    Create html and css for the home page.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var addDressOne = document.getElementById('one');
+var addDressTwo = document.getElementById('two');
+
+var addTopsOne = document.getElementById('three');
+var addTopsTwo = document.getElementById('four');
+
+var addOuterwearOne = document.getElementById('five');
+var addOuterwearTwo = document.getElementById('six');
+
+var addDress = function() {
+  addDressOne.
+}
