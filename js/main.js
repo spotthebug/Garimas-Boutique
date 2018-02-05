7c7f21559b4e5d59f6e0f6d25753e43983052465
spotthebug <gjain1@scu.edu>
2018-02-05 09:46:31 -0800

    Added dynamica loading functionality of elements.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+var dressLink = document.getElementById('add-dresses');
+var topLink = document.getElementById('add-tops');
+var outerwearLink = document.getElementById('add-outerwear');
+
 var addDressOne = document.getElementById('one');
 var addDressTwo = document.getElementById('two');
 
@@ -7,6 +11,34 @@ var addTopsTwo = document.getElementById('four');
 var addOuterwearOne = document.getElementById('five');
 var addOuterwearTwo = document.getElementById('six');
 
-var addDress = function() {
-  addDressOne.
-}
+dressLink.addEventListener("click", function() {
+  addDressOne.innerHTML += '<img class="image-sizing"src="../assets/bluecocktail.jpg" alt="Black dress with contrasting trimmings">';
+  addDressOne.innerHTML += '<button class="button">Add To Cart</button>';
+  addDressOne.innerHTML += '<img class="image-sizing"src="../assets/blueprom.jpg" alt="White colored prom dress with blue lace bodice.">';
+  addDressOne.innerHTML += '<button class="button">Add To Cart</button>';
+  addDressOne.innerHTML += '<img class="image-sizing"src="../assets/floraldress.jpg" alt="Chic floral dress.">';
+  addDressOne.innerHTML += '<button class="button">Add To Cart</button>';
+  addDressTwo.innerHTML += '<img class="image-sizing"src="../assets/promdress.jpg" alt="Prom dress">';
+  addDressTwo.innerHTML += '<button class="button">Add To Cart</button>';
+  addDressTwo.innerHTML += '<img class="image-sizing"src="../assets/shiftdress.jpg" alt="Black shift dress">';
+  addDressTwo.innerHTML += '<button class="button">Add To Cart</button>';
+  addDressTwo.innerHTML += '<img class="image-sizing"src="../assets/denimdress.jpg" alt="Yellow dress">';
+  addDressTwo.innerHTML += '<button class="button">Add To Cart</button>';
+});
+
+topLink.addEventListener("click", function() {
+  addTopsOne.innerHTML += '<img class="image-sizing"src="../assets/pearls.jpg" alt="Blue top with pearls.">';
+  addTopsOne.innerHTML += '<button class="button">Add To Cart</button>';
+  addTopsOne.innerHTML += '<img class="image-sizing"src="../assets/blackcroptop.jpg" alt="Black crop top.">';
+  addTopsOne.innerHTML += '<button class="button">Add To Cart</button>';
+  addTopsOne.innerHTML += '<img class="image-sizing"src="../assets/CamisaChiffon.jpg" alt="Chiffon top.">';
+  addTopsOne.innerHTML += '<button class="button">Add To Cart</button>';
+  addTopsTwo.innerHTML += '<img class="image-sizing"src="../assets/blousesv-neck.jpg" alt="V-neck blouse">';
+  addTopsTwo.innerHTML += '<button class="button">Add To Cart</button>';
+  addTopsTwo.innerHTML += '        <img class="image-sizing"src="../assets/peachblouse.jpg" alt="Peach blouse.">';
+  addTopsTwo.innerHTML += '<button class="button">Add To Cart</button>';
+  addTopsTwo.innerHTML += '<img class="image-sizing"src="../assets/limegreentop.jpg" alt="Lime green top.">';
+  addTopsTwo.innerHTML += '<button class="button">Add To Cart</button>';
+});
+
+
